24bb83b14d6e738584255cdd1c210868561d6830
Frank Alkema <frank.alkema@icloud.com>
2018-11-11 10:21:45 +0100

    final changes

--- source/javascripts/typed.js
+++ source/javascripts/typed.js
@@ -1,1 +1,15 @@
 
+// import Typed from 'typed.js';
+
+
+document.addEventListener('DOMContentLoaded', function(event){
+  function loadDynamicBannerText() {
+    new Typed('.test', {
+      strings: ["Ruby on Rails", "JavaScript","HTML","CSS","SQL"],
+      typeSpeed: 70,
+      loop: true,
+      showCursor: false
+    });
+  }
+  loadDynamicBannerText();
+})
